ba336e5a01020f78bb9510fd510a0c7772285c78
Ru-kko <verase2003.1@gmail.com>
2023-04-10 20:03:05 -0500

    moved class 12

--- src/structs/Clase12/BinarySearch.java
+++ src/structs/Clase12/BinarySearch.java
@@ -1,11 +1,11 @@
-package structs;
+package structs.Clase12;
 
-public class Clase12 {
+public class BinarySearch {
 
   public static void main(String[] args) throws Exception {
     Integer[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
     Integer toFind = 10;
-    System.out.println(Clase12.Find(toFind, array));
+    System.out.println(BinarySearch.Find(toFind, array));
   }
 
   public static Integer Find(Integer num, Integer[] array) {
