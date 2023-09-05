4368759b2fcdf8ecf47a392ea819b28984e9257e
Ru-kko <verase2003.1@gmail.com>
2023-09-05 18:08:51 -0500

    taller 3

---
+++ Ejercicio_03/ejercio/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace App
+{
+  class Entry
+  {
+    static void Main()
+    {
+      Console.WriteLine(factorial(5));
+      /* Console.WriteLine("Ingrese el primer numero"); */
+      /* int a = Convert.ToInt32(Console.ReadLine()); */
+      /* Console.WriteLine("Ingrese el segundo numero"); */
+      /* int b = Convert.ToInt32(Console.ReadLine()); */
+      /* Console.WriteLine($"La suma de {a} + {b} es: {a + b}"); */
+    }
+
+    static int factorial(int n)
+    {
+      if (n == 1)
+      {
+        return 1;
+      }
+
+      return n * factorial(n - 1);
+    }
+  }
+}
+
+/*
+* Pedir dos numer y diferenciar menor y mayor
+* Pedir el nombre de un dia de la semana X
+* Pedir el precio de un producto, la forma de pago, y si es pago en targeta pedir el numero de cuenta
+*/
---
+++ Ejercicio_03/standars/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Application
+{
+  class App
+  {
+    static void Main()
+    {
+      string apellidos;
+      string nombres;
+      int edad;
+      double estatura;
+      bool estudiante;
+      double salario;
+      int identificacion;
+
+      Console.WriteLine("Digite el numero de identificacion");
+      identificacion = int.Parse(Console.ReadLine());
+      Console.WriteLine("Digite los apellidos");
+      apellidos = Console.ReadLine();
+      Console.WriteLine("Digite los nombres");
+      nombres = Console.ReadLine(); 
+      Console.WriteLine("Digite la edad");
+      edad = int.Parse(Console.ReadLine());
+      Console.WriteLine("Digite su estatura");
+      estatura = double.Parse(Console.ReadLine());
+      Console.WriteLine("Digite el salario");
+      salario = double.Parse(Console.ReadLine());
+      Console.WriteLine("Es estudiante? (true o false) ");
+      if (!bool.TryParse(Console.ReadLine(),out estudiante)) 
+      {
+        Console.WriteLine("Valor no valido, se asumira como false");
+        estudiante = false;
+      }
+
+      Console.WriteLine($"Identificacion: {identificacion}");
+      Console.WriteLine($"Apellidos: {apellidos}");
+      Console.WriteLine($"Nombres: {nombres}");
+      Console.WriteLine($"Edad: {edad}");
+      Console.WriteLine($"Estatura: {estatura}");
+      Console.WriteLine($"Salario: {salario}");
+      Console.WriteLine(estudiante ? "Es estudiante" : "No es estudiante");
+
+      Console.ReadKey();
+    }
+     /*
+      * Ingresar dos numeros y realizar la suma
+      * */
+
+    static bool readBoolLine()
+    {
+      string line = Console.ReadLine().ToLower();
+
+      if (line == "true" || line == "1" || line == "verdadero" || line == "si")
+      {
+        return true;
+      }
+      if (line == "false" || line == "0" || line == "false" || line == "no")
+      {
+        return false;
+      }
+
+      throw new FormatException();
+    }
+  }
+}
