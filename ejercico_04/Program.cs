9bca7eb1dc873f1445fd659edd2e453423929dca
Ru-kko <verase2003.1@gmail.com>
2023-09-26 18:00:55 -0500

    ejercicio 4

---
+++ ejercico_04/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace App;
+
+class Application
+{
+  static string getString() {
+    var txt = Console.ReadLine();
+    try
+    {
+      Convert.ToDecimal(txt);
+    }
+    catch (FormatException)
+    {
+      return txt != null ? txt : "";
+    }
+    return "texto no valido";
+  }
+
+  static int getInteger()
+  {
+    var txt = Console.ReadLine();
+    try
+    {
+      return Convert.ToInt32(txt);
+    }
+    catch (FormatException)
+    {
+      Console.WriteLine(txt + " no es un numero valido, intente de nuevo: ");
+      return getInteger();
+    }
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine("Ingrese un numero entero: ");
+    Console.WriteLine("Se ecribio el el numero: " + Application.getInteger());
+  }
+}
